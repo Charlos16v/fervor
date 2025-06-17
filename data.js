e5cf076739f1b6a209d66aa837f0fda0849218d9
Carlos Uriel <cudominguez@idealista.com>
2025-06-17 10:35:02 +0200

    refactor: relocate images

--- data.js
+++ data.js
@@ -3,18 +3,18 @@ const revistas = [
     id: 1,
     titulo: "FERVOR Nº3",
     fecha: "06.2024",
-    imagen: "portadas/fervor_3.jpg"
+    imagen: "assets/portadas/fervor_3.jpg"
   },
   {
     id: 2,
     titulo: "FERVOR Nº2",
     fecha: "06.2023",
-    imagen: "portadas/fervor_2.jpg"
+    imagen: "assets/portadas/fervor_2.jpg"
   },
   {
     id: 3,
     titulo: "FERVOR Nº1",
     fecha: "06.2022",
-    imagen: "portadas/fervor_1.jpg"
+    imagen: "assets/portadas/fervor_1.jpg"
   }
 ];
